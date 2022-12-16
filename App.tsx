07bbe13ee37fbc0342480811c466a07de2a38eb1
Zachary Traczyk <cookies951@gmail.com>
2022-12-15 20:31:37 -0600

    tailwind added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={[tw`h-full flex flex-col justify-center items-center`]}>
+      <Text>Open up App.tsx work on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// Tailwind Config File
+// -------------------
+// "The theme section is where you define your color palette, font stacks, type
+// scale, border sizes, breakpoints — anything related to the visual design of
+// your site."
+
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: "",
+      body: "",
+    },
+  },
+};
