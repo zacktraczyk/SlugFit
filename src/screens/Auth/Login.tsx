1d757853c0ce1d1ea68a9f49f1d98db18352c6ff
Shaan <shaanmistry03@gmail.com>
2023-01-25 12:50:40 -0800

    edited tailwind components

--- src/screens/Auth/Login.tsx
+++ src/screens/Auth/Login.tsx
@@ -63,13 +63,19 @@ const Login: React.FC<LoginProps> = ({ navigation }) => {
     <View className="flex h-full flex-col items-stretch justify-center p-5">
       <View>
       <Text style={{ textAlign: 'center', fontFamily: 'BebasNeue', fontSize: 96}}>SlugFit</Text>
-      <Text style={{ textAlign: 'center', fontFamily: 'BebasNeue', fontSize: 24 }}>We see mass in your future</Text>
-      </View>
+      <Text style={{ lineHeight: 24 * 0.3, 
+                    paddingTop: 24 - (24 * 0.3), 
+                    paddingBottom: 50,
+                    textAlign: 'center', 
+                    fontFamily: 'BebasNeue', 
+                    fontSize: 24,
+                    color: '#3C3A3A' }}>We see mass in your future</Text>
+    </View>
       <KeyboardAvoidingView behavior="padding">
         <TouchableWithoutFeedback accessibilityRole="button" onPress={Keyboard.dismiss}>
           <View className="flex h-96 flex-col items-stretch justify-evenly">
-            <View>
-            <Text style={{ fontFamily: 'BebasNeue', fontSize: 30 }}>Email</Text>
+            <View style={{}}>
+            <Text style={{ fontFamily: 'BebasNeue', fontSize: 18 }}>Email</Text>
               <Controller
                 control={control}
                 rules={{
@@ -77,10 +83,11 @@ const Login: React.FC<LoginProps> = ({ navigation }) => {
                   pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                 }}
                 render={({ field: { onChange, onBlur, value } }) => (
+                  
                   <TextInput
                     accessibilityLabel="Text input field"
                     accessibilityHint="Input email for login"
-                    className="mb-2 border-b-2 text-xl"
+                    className="my-2 mt-0 w-80 rounded-md bg-white-500 border-2 border-gray-200 p-2 text-xl"
                     autoCapitalize="none"
                     keyboardType="email-address"
                     returnKeyType="next"
@@ -100,7 +107,7 @@ const Login: React.FC<LoginProps> = ({ navigation }) => {
             </View>
 
             <View>
-            <Text style={{ fontFamily: 'BebasNeue', fontSize: 30 }}>Password</Text>
+            <Text style={{ fontFamily: 'BebasNeue', fontSize: 18 }}>Password</Text>
               <Controller
                 control={control}
                 rules={{
@@ -111,7 +118,7 @@ const Login: React.FC<LoginProps> = ({ navigation }) => {
                   <TextInput
                     accessibilityLabel="Text input field"
                     accessibilityHint="Input password for login"
-                    className="mb-2 border-b-2 text-xl"
+                    className="my-2 mt-0 w-80 rounded-md bg-white-500 border-2 border-gray-200 p-2 text-xl"
                     secureTextEntry={true}
                     onBlur={onBlur}
                     onChangeText={onChange}
@@ -132,14 +139,16 @@ const Login: React.FC<LoginProps> = ({ navigation }) => {
               <Pressable
                 accessibilityRole="button"
                 accessibilityHint="Login to application"
-                className="my-2 w-60 rounded bg-red-400 p-2"
+                className="my-2 mt-0 w-60 rounded-lg bg-red-500 p-2"
                 onPress={handleSubmit((data) => onSubmit(data))}
               >
                 <Text style={{ textAlign: 'center', color: 'white', fontFamily: 'BebasNeue', fontSize: 24 }}>Sign In</Text>
               </Pressable>
-
-              <Text className="py-2" onPress={() => navigation.navigate('RegisterScreen')}>
-                Don&apos;t Have an Account? Create One
+              <Text>
+                <Text>Don&apos;t Have an Account?
+                </Text>
+                <Text style={{fontWeight: "bold"}} className="py-2" onPress={() => navigation.navigate('RegisterScreen')}>{' '}Create One
+                </Text>
               </Text>
             </View>
           </View>
