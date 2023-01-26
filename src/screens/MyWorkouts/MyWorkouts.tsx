359f9d83bd966fa132f84825fb7f82c99e6be145
pajsimbulan <pajsimbulan@gmail.com>
2023-01-26 14:10:31 -0800

    connected to supabase

--- src/screens/MyWorkouts/MyWorkouts.tsx
+++ src/screens/MyWorkouts/MyWorkouts.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Text, View } from 'react-native';
 import { NavigatorParamList } from '../DrawerNavigator';
 import SearchBarWorkout from './WorkoutSearchBar';
+import { SearchBarWorkoutProps } from './WorkoutSearchBar';
 
 type MyWorkoutsProps = NativeStackScreenProps<NavigatorParamList, 'MyWorkouts'>;
 
 const MyWorkouts: React.FC<MyWorkoutsProps> = () => {
+  const exerciseSelected = React.useRef({exercise: "", isOk:false}); 
+
   return (
     <View className="flex h-full flex-col items-center ">
-      <SearchBarWorkout />
+      <SearchBarWorkout  selectedExercise={exerciseSelected.current} />
     </View>
   );
 };
--- src/screens/MyWorkouts/WorkoutSearchBar.tsx
+++ src/screens/MyWorkouts/WorkoutSearchBar.tsx
@@ -2,37 +2,59 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useEffect, useState, useRef } from 'react';
 import { View, Text, Button, Touchable, TouchableOpacity} from 'react-native';
 import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import { SearchBarProps } from 'react-native-screens';
 import { supabase } from '../../utils/supabaseClient';
 
+/**
+ * @param selectedExercise.exercise //value set to exercise pressed
+ * @param selectedExercise.isOk //if true, then value of exercise is valid, otherwise disregard
+ */
+export type SearchBarWorkoutProps = {
+    selectedExercise: {exercise: string | undefined, isOk:boolean | undefined}
+};
 
-
-export const SearchBarWorkout: React.FC = () => {
-    const [recentlyUsed, setRecentlyUsed] = useState(["Lat Pulldowns", "Barbell Row"]);
-    const [allExercises, setAllExercises] = useState(["Deadlift", "Squat", "Bench press", "Barbell Row", "Shoulder Press", "Pull-ups", "Dips", "Bicep Curls", "Tricep Extensions", "Lateral Raises", "Lat Pulldowns"]);
-    const [isSearching, setIsSearching] = useState(0);
+const SearchBarWorkout: React.FC<SearchBarWorkoutProps> = (prop: SearchBarWorkoutProps) => {
+    const [allExercises, setAllExercises] = useState<string[]>([]);
+    const [searchInput, setSearchInput] = useState("");
+    const recentlyUsedExercise = useRef("");
     const searchArray = useRef(new Array());
-
-    const searchResult = (inputText: string) => {
-        if(inputText.length) {
-            searchArray.current = new Array();
-            let tempArr = allExercises.filter(exercise => exercise.toLowerCase().includes(inputText.toLowerCase()));
-            tempArr.map(exercise => {searchArray.current.push(exercise)});
-            setIsSearching(isSearching+1);
-        } else {
-            setIsSearching(0);
-        }
-    };
-
     
     
+    if(searchInput.length > 0) {
+        searchArray.current = new Array();
+        let tempArr = allExercises.filter(exercise => exercise.toLowerCase().includes(searchInput.toLowerCase()));
+        tempArr.map(exercise => {searchArray.current.push(exercise)}); 
+    }
+    
+    async function fetchExercises () {
+        let { data, error } = await supabase
+            .from('exercises')
+            .select('*');
+            if(error) {
+                alert(error);
+            }
+            if(data != null) {
+                let temp:string[] = [];
+                data.map((exercise) => {temp.push(exercise.name)});
+                setAllExercises(temp);
+            }
+    }
 
+    useEffect(() => {
+       fetchExercises();
+    },[]);
 
-    const onClickHandler = (event, index) => {
-        
+    const onPressHandler = (item: string) => {
+        recentlyUsedExercise.current = item;
+        prop.selectedExercise.exercise = item;
+        prop.selectedExercise.isOk = true;
+        // <<----- Code to navigate back to parents page goes back here ----->>
+        //
+        //
     }
 
     return (
-    <View className="h-screen bg-slate-50  w-full items-center shadow-sm">
+    <View className="bg-slate-50 h-full w-full items-center ">
         <View className="mt-7 h-12  w-11/12 bg-slate-50  rounded-t border-x border-t border-slate-200">
         <TextInput
                     
@@ -42,32 +64,37 @@ export const SearchBarWorkout: React.FC = () => {
                     autoCapitalize="none"
                     placeholder="Search for an exercise"
                     returnKeyType="next"
-                    onChangeText={searchResult}
+                    value={searchInput}
+                    onChangeText={(value) => {setSearchInput(value)}}
         />          
         </View> 
         <View className="h-60 w-11/12  bg-slate-50 divide-y-10 border border-slate-200 overflow-scroll invisible">
             <ScrollView>
-                {(!isSearching)? 
+                {(searchInput.length <= 0)? 
                 <View className='mx-5 '>
-                <Text className="mx-1 my-3 text-l font-light">Recently Used</Text>
-                { recentlyUsed.map( (item) => {return (
-                <TouchableOpacity key={'recently-used-' + item} className="p-3 border border-slate-200 rounded mb-1">
-                    <Text className="font-bold text-l ">
-                        {item}
-                    </Text>
-                </TouchableOpacity>)})}
-                <Text className="mx-1 my-3 text-l font-light">All Exercises</Text>
+                    {(recentlyUsedExercise.current.length >0)?
+                        <View>
+                            <Text className="mx-1 my-3 text-l font-light">Recently Used</Text>
+                            <TouchableOpacity key={'recently-used-' + recentlyUsedExercise.current} onPress={() => onPressHandler(recentlyUsedExercise.current)} className="p-3 border border-slate-200 rounded mb-1">
+                                <Text className="font-bold text-l ">
+                                    {recentlyUsedExercise.current}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>  
+                    : <View /> 
+                    } 
+                <Text className="mx-1 my-3 text-l font-light ">All Exercises</Text>
                 {allExercises.map( (item) => {return (
-                <TouchableOpacity key={item} className="p-3 border border-slate-200 rounded mb-1">
+                <TouchableOpacity key={item} onPress={() => onPressHandler(item)} className="p-3 border border-slate-200 rounded mb-1">
                     <Text className="font-bold text-l">
                         {item}
                     </Text>
                 </TouchableOpacity>)})}
                 </View> : 
                     <View className='mx-5 '>
-                        <Text className="my-1 text-xs font-light"> Search Result:</Text>
+                        <Text className="my-1 text-xs font-light "> Search Result:</Text>
                         {searchArray.current.map( (item) => {return (
-                        <TouchableOpacity key={item} className="p-3 border border-slate-200 rounded mb-1">
+                        <TouchableOpacity key={item} onPress={() => onPressHandler(item)} className="p-3 border border-slate-200 rounded mb-1">
                             <Text className="font-bold text-l ">
                                 {item}
                             </Text>
@@ -78,3 +105,5 @@ export const SearchBarWorkout: React.FC = () => {
         </View>     
     </View>);
 }
+
+export default SearchBarWorkout;
