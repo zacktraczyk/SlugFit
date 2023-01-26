57a85068cd9ec86909bf35ff3c7724a51b38035c
pajsimbulan <pajsimbulan@gmail.com>
2023-01-25 17:22:36 -0800

    temp

--- src/screens/MyWorkouts/MyWorkouts.tsx
+++ src/screens/MyWorkouts/MyWorkouts.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Text, View } from 'react-native';
 import { NavigatorParamList } from '../DrawerNavigator';
+import SearchBarWorkout from './WorkoutSearchBar';
 
 type MyWorkoutsProps = NativeStackScreenProps<NavigatorParamList, 'MyWorkouts'>;
 
 const MyWorkouts: React.FC<MyWorkoutsProps> = () => {
   return (
-    <View className="flex h-full flex-col items-center justify-center">
-      <Text>MyWorkouts</Text>
+    <View className="flex h-full flex-col items-center ">
+      <SearchBarWorkout />
     </View>
   );
 };
